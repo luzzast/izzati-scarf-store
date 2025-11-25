1a6f2169f60118b2a9075bba804dab5357b29cb6
Luqman Hakim <81523591+luzzast@users.noreply.github.com>
2025-11-25 14:26:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+// src/App.jsx - UPDATED WITH NEW ROUTES
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";  // ← Add this
-import ScarfStore from "./components/ScarfStore";
+import LandingPage from "./pages/LandingPage";
+import ViewSelector from "./pages/ViewSelector";  // ← NEW
+import ScarfStore from "./components/ScarfStore";  // Will be desktop version
+import ScarfStoreMobile from "./components/ScarfStoreMobile";  // ← NEW (we'll create next)
 import ThankYou from "./pages/ThankYou";
 import { Toaster } from "react-hot-toast";
 
@@ -11,8 +14,11 @@ function App() {
       <Toaster position="top-right" reverseOrder={false} />
 
       <Routes>
-        <Route path="/" element={<LandingPage />} />  {/* ← Landing page */}
-        <Route path="/shop" element={<ScarfStore />} />  {/* ← Shop page */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/view-selector" element={<ViewSelector />} />  {/* ← NEW */}
+        <Route path="/shop-desktop" element={<ScarfStore />} />  {/* ← Desktop view */}
+        <Route path="/shop-mobile" element={<ScarfStoreMobile />} />
+        <Route path="/shop" element={<ScarfStore />} />  {/* ← Keep for backward compatibility */}
         <Route path="/thank-you" element={<ThankYou />} />
       </Routes>
     </div>
